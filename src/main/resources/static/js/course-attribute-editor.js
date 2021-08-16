c2053341c2b76218fb27831c0a5f0a944405e69b
Gregory J Thomas <gjthomas@iu.edu>
2021-08-16 16:51:02 -0400

    LMSA-7620 - all buttons disabled when clicked

--- src/main/resources/static/js/course-attribute-editor.js
+++ src/main/resources/static/js/course-attribute-editor.js
@@ -1,6 +1,9 @@
 function buttonLoading(button) {
     button.setAttribute("aria-busy", true);
-    button.disabled = true;
+    var buttonsToDisable = document.getElementsByTagName('button');
+    for(var i = 0; i < buttonsToDisable.length; i++) {
+        buttonsToDisable[i].disabled = true;
+    }
     button.classList.add("rvt-button--loading");
     button.getElementsByTagName('div')[0].classList.remove("hideMe");
 }
